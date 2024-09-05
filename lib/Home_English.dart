ef9cc87ebe611b6ce1e028e551d9b284b9c44d09
Foss1lz <setharichard7777@gmail.com>
2024-09-05 11:30:07 +0700

    idk

--- lib/Home_English.dart
+++ lib/Home_English.dart
@@ -112,7 +112,7 @@ class _Home_EnglishState extends State<Home_English> {
         toolbarHeight: 55,
         backgroundColor: const Color.fromARGB(255, 30, 96, 144),
         flexibleSpace: Container(
-          padding: EdgeInsets.only(top: 20),
+          padding: const EdgeInsets.only(top: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
